1f4ef22114cdf2104ea5d1480571708cc153fc1c
Vitaliygit021 <v.goncharov@tg.dunice.net>
2024-08-26 12:43:40 +0300

    Добавленно шифрование пароля при регистрации. А также генерация токена. И написанна валидация в RegistrationUserDtoRequest

--- src/main/java/com/dunice/GoncharovVVAdvancedServer/dto/request/RegistrationUserDtoRequest.java
+++ src/main/java/com/dunice/GoncharovVVAdvancedServer/dto/request/RegistrationUserDtoRequest.java
@@ -1,5 +1,11 @@
 package com.dunice.GoncharovVVAdvancedServer.dto.request;
 
+
+import com.dunice.GoncharovVVAdvancedServer.constants.ValidationConstants;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,15 +13,24 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Valid
 public class RegistrationUserDtoRequest {
+
     private String avatar;
 
+    @Email(message = ValidationConstants.USER_EMAIL_NOT_VALID)
+    @NotBlank(message = ValidationConstants.USER_EMAIL_NOT_NULL)
     private String email;
 
+    @Size(min = 3, max = 25, message = ValidationConstants.USERNAME_SIZE_NOT_VALID)
+    @NotBlank(message = ValidationConstants.USER_NAME_HAS_TO_BE_PRESENT)
     private String name;
 
+    @Size(min = 3, max = 25, message = ValidationConstants.PASSWORD_NOT_VALID)
+    @NotBlank(message = ValidationConstants.USER_PASSWORD_NULL)
     private String password;
 
+    @Size(min = 3, max = 100, message = ValidationConstants.ROLE_SIZE_NOT_VALID)
     private String role;
 
 }
