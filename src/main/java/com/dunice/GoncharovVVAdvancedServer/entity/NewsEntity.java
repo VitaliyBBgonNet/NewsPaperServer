f0d82a12c9ca5852070defa4b303e8a702c2693d
Vitaliygit021 <v.goncharov@tg.dunice.net>
2024-09-03 09:48:50 +0300

    Refactoring code

--- src/main/java/com/dunice/GoncharovVVAdvancedServer/entity/NewsEntity.java
+++ src/main/java/com/dunice/GoncharovVVAdvancedServer/entity/NewsEntity.java
@@ -1,10 +1,15 @@
 package com.dunice.GoncharovVVAdvancedServer.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
-
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @Entity
